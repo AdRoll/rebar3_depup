377e136ec018dc4c67669771370c9bfef79c2cad
josh rotenberg <joshrotenberg@users.noreply.github.com>
2021-10-25 13:39:29 -0700

    only updates

--- test/depup_SUITE.erl
+++ test/depup_SUITE.erl
@@ -154,29 +154,68 @@ ignore_config(_) ->
 
 %% @doc Update if the patch version is the only change
 only_patch(_) ->
+    meck:new(dep_hex, [passthrough]),
+    meck:expect(dep_hex,
+                get_latest_vsn,
+                %% will update from 1.1.1 to 1.1.3
+                fun (will_update) ->
+                        <<"1.1.3">>;
+                    %% won't update from 1.1.1 to 1.3.3
+                    (wont_update) ->
+                        <<"1.3.3">>;
+                    (Name) ->
+                        meck:passthrough([Name])
+                end),
     {OriginalConfig, UpdatedConfig} =
         run_with("only_patch.config", [{replace, true}, {just_deps, true}, {only, patch}]),
-    [{recon, _}, {spillway, _}] =
+    [{will_update, _}] =
         lists:usort(proplists:get_value(deps, UpdatedConfig)
                     -- proplists:get_value(deps, OriginalConfig)),
+    meck:unload(dep_hex),
     ok.
 
-%% @doc Update if the minor version is the only change
+%% @doc Update if the minor (or patch) version is the only change
 only_minor(_) ->
+    meck:new(dep_hex, [passthrough]),
+    meck:expect(dep_hex,
+                get_latest_vsn,
+                %% will update from 1.1.1 to 1.2.1
+                fun (will_update) ->
+                        <<"1.2.1">>;
+                    %% won't update from 1.1.1 to 2.1.1
+                    (wont_update) ->
+                        <<"2.1.1">>;
+                    (Name) ->
+                        meck:passthrough([Name])
+                end),
     {OriginalConfig, UpdatedConfig} =
         run_with("only_minor.config", [{replace, true}, {just_deps, true}, {only, minor}]),
-    [{recon, _}, {spillway, _}] =
+    [{will_update, _}] =
         lists:usort(proplists:get_value(deps, UpdatedConfig)
                     -- proplists:get_value(deps, OriginalConfig)),
+    meck:unload(dep_hex),
     ok.
 
 %% @doc Update only if the version follows SemVer semantics
 only_major(_) ->
+    meck:new(dep_hex, [passthrough]),
+    meck:expect(dep_hex,
+                get_latest_vsn,
+                %% will update from 1.1.1 to 2.2.1
+                fun (will_update) ->
+                        <<"2.2.1">>;
+                    %% won't update from 1.1.1 to unparsable version
+                    (wont_update) ->
+                        <<"not-semver">>;
+                    (Name) ->
+                        meck:passthrough([Name])
+                end),
     {OriginalConfig, UpdatedConfig} =
         run_with("only_major.config", [{replace, true}, {just_deps, true}, {only, major}]),
-    [{recon, _}, {spillway, _}] =
+    [{will_update, _}] =
         lists:usort(proplists:get_value(deps, UpdatedConfig)
                     -- proplists:get_value(deps, OriginalConfig)),
+    meck:unload(dep_hex),
     ok.
 
 full_path(RebarConfig) ->
